e37ef05292483b7a2bd86109ac6a961dfd431873
matt11matthew <matt11matthew@gmail.com>
2025-06-20 16:16:15 -0400

    Started

---
+++ src/main/java/me/matthewe/test/trialcore/TrialCore.java
@@ -0,0 +1,17 @@
+package me.matthewe.test.trialcore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TrialCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
